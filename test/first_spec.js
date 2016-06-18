0285a61970b88ad990a06f02ac5074ce1564d4e8
Ramesh Pettikattil govindankutty <rameshpg@Rameshs-MacBook-Pro.local>
2016-06-18 09:56:24 -0700

    initial commit

---
+++ test/first_spec.js
@@ -0,0 +1,11 @@
+///</// <reference path="typings/index.d.ts" />
+
+var assert = require('assert');
+
+describe('A feature', function(){
+    describe('A Scenario', function(){
+        it("Does something expected", function(){
+            assert(true);
+        })
+    });
+});
